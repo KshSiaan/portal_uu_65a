3afac06c62b1d6da7efab2262d95eed7ac0c3c45
KshSiaan <kshsiaan@gmail.com>
2025-09-28 00:43:46 +0600

    .

--- src/components/core/navbar.tsx
+++ src/components/core/navbar.tsx
@@ -20,32 +20,28 @@ import {
   SheetTitle,
   SheetTrigger,
 } from "../ui/sheet";
+
 const navs = [
+  {
+    title: "Class routine",
+    to: "/routine",
+    hasChild: false,
+  },
   {
     title: "Notices",
     to: "/notices",
     hasChild: false,
   },
   {
-    title: "Class routine",
-    to: "/routine",
-    hasChild: false,
-  },
-  // {
-  //   title: "Semester Essentials",
-  //   to: "/essentials",
-  //   hasChild: false,
-  // },
-  {
     title: "Links & Resources",
     to: "/resources",
     hasChild: false,
   },
-  // {
-  //   title: "FAQ",
-  //   to: "/faq",
-  //   hasChild: false,
-  // },
+  {
+    title: "FAQ",
+    to: "/faq",
+    hasChild: false,
+  },
   {
     title: "Chat / Group Links",
     to: "/chat",
@@ -69,9 +65,11 @@ const navs = [
     ],
   },
 ];
+
 export default function Navbar() {
   return (
     <nav className="flex flex-row justify-between w-full gap-4 lg:gap-6">
+      {/* Logo (desktop) */}
       <Card className="bg-foreground dark:bg-card hidden lg:block h-16 lg:aspect-[6/2]! w-full lg:w-min p-3! order-2">
         <Image
           src={"/logo.png"}
@@ -82,72 +80,76 @@ export default function Navbar() {
           className="object-contain w-full h-full!"
         />
       </Card>
+
+      {/* Navbar desktop */}
       <Card className="order-2 hidden xl:flex h-16 flex-1 rounded-lg p-2! w-full flex-row justify-center items-center border-foreground dark:border-border">
-        <NavigationMenu className="">
-          <NavigationMenuList className="">
-            {navs.map((x) =>
-              x.hasChild ? (
-                <NavigationMenuItem key={x.title} className="relative!">
-                  <NavigationMenuTrigger>{x.title}</NavigationMenuTrigger>
-                  <NavigationMenuContent className="w-full! absolute h-[20dvh]">
-                    <div className="w-full grid grid-cols-2 p-2 gap-4 h-full">
-                      <Card className="w-full max-w-lg mx-auto p-4 grid gap-2 text-center">
-                        <div className="flex items-center justify-center">
-                          <Image
-                            src="/logo.png"
-                            alt="UU Logo"
-                            width={300}
-                            height={100}
-                            draggable={false}
-                            className="object-contain max-h-16"
-                          />
-                        </div>
-                        <p className="text-sm text-muted-foreground">
-                          Visit the{" "}
-                          <Link
-                            href="http://uttarauniversity.edu.bd"
-                            target="_blank"
-                            rel="noopener noreferrer"
-                            className="font-medium text-primary underline-offset-4 hover:underline"
-                          >
-                            Official UU Website
-                          </Link>
-                        </p>
-                      </Card>
-                      <div className="w-full h-fit grid grid-cols-2 gap-4">
-                        {x.hasChild &&
-                          x.childs?.map((x) => (
-                            <Button variant={"secondary"} key={x.to} asChild>
-                              <Link href={x.to}>{x.title}</Link>
+        <NavigationMenu>
+          <NavigationMenuList>
+            {navs.map((x) => (
+              <NavigationMenuItem key={x.title} className="relative!">
+                {x.hasChild ? (
+                  <>
+                    <NavigationMenuTrigger>{x.title}</NavigationMenuTrigger>
+                    <NavigationMenuContent className="w-full! absolute h-[20dvh]">
+                      <div className="w-full grid grid-cols-2 p-2 gap-4 h-full">
+                        <Card className="w-full max-w-lg mx-auto p-4 grid gap-2 text-center">
+                          <div className="flex items-center justify-center">
+                            <Image
+                              src="/logo.png"
+                              alt="UU Logo"
+                              width={300}
+                              height={100}
+                              draggable={false}
+                              className="object-contain max-h-16"
+                            />
+                          </div>
+                          <p className="text-sm text-muted-foreground">
+                            Visit the{" "}
+                            <Link
+                              href="http://uttarauniversity.edu.bd"
+                              target="_blank"
+                              rel="noopener noreferrer"
+                              className="font-medium text-primary underline-offset-4 hover:underline"
+                            >
+                              Official UU Website
+                            </Link>
+                          </p>
+                        </Card>
+                        <div className="w-full h-fit grid grid-cols-2 gap-4">
+                          {x.childs?.map((child) => (
+                            <Button variant="secondary" key={child.to} asChild>
+                              <Link href={child.to}>{child.title}</Link>
                             </Button>
                           ))}
+                        </div>
                       </div>
-                    </div>
-                  </NavigationMenuContent>
-                </NavigationMenuItem>
-              ) : (
-                <NavigationMenuItem key={x.title}>
+                    </NavigationMenuContent>
+                  </>
+                ) : (
                   <NavigationMenuLink
                     asChild
                     className={navigationMenuTriggerStyle()}
                   >
                     <Link href={x.to!}>{x.title}</Link>
                   </NavigationMenuLink>
-                </NavigationMenuItem>
-              )
-            )}
+                )}
+              </NavigationMenuItem>
+            ))}
           </NavigationMenuList>
         </NavigationMenu>
       </Card>
 
+      {/* Batch info */}
       <Card className="w-full bg-foreground text-background dark:bg-card dark:text-foreground lg:w-auto text-xs order-1 lg:order-3 lg:text-base h-16 aspect-[4/2] lg:aspect-[6/2] p-2! gap-0! text-center! flex justify-center items-center font-semibold">
         <h4>65A Eve</h4>
         <p className="text-amber-400">The Golden Batch</p>
       </Card>
+
+      {/* Mobile menu */}
       <Card className="lg:hidden order-3 aspect-[4/2]! flex items-center justify-center p-0!">
         <Sheet>
           <SheetTrigger asChild>
-            <Button className="w-full h-full!" variant={"ghost"}>
+            <Button className="w-full h-full!" variant="ghost">
               <MenuIcon />
             </Button>
           </SheetTrigger>
